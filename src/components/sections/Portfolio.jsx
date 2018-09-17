1a3800b95e3f3b3135052cebded220cd3d38e12a
William Whatley <william@catatek.com>
2018-09-17 19:24:41 -0400

    Added flickering animation.

--- src/components/sections/Portfolio.jsx
+++ src/components/sections/Portfolio.jsx
@@ -61,6 +61,7 @@ const Div = styled.div`
   margin: 1em;
   border-radius: 3px;
   cursor: pointer;
+  box-shadow: 2px 4px 4px rgba(0, 0, 0, 0.25);
 `;
 
 function PortfolioItem({ title, description }) {
